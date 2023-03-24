d9e365771f568d839465a3543638804b0b510a14
FaustianDeal <cdorff@gmail.com>
2023-03-24 12:35:34 -0400

    chat bubble adjst

--- Assets/Scripts/ChatBubble.cs
+++ Assets/Scripts/ChatBubble.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class ChatBubble : MonoBehaviour
 {
     public List<TextMeshProUGUI> TextAreas;
 
+    private RectTransform rectTransform;
+    private Rect safeArea;
+    private RectTransform safeAreaRectTransform;
+
+    private bool initialized = false;
+    void Initialize()
+    {
+        rectTransform = GetComponent<Image>().GetComponent<RectTransform>();
+        safeArea = Screen.safeArea;
+        safeAreaRectTransform = new GameObject("SafeArea", typeof(RectTransform)).GetComponent<RectTransform>();
+        safeAreaRectTransform.SetParent(rectTransform.parent);
+        safeAreaRectTransform.position = new Vector2(Screen.safeArea.x + Screen.safeArea.width / 2, Screen.safeArea.y + Screen.safeArea.height / 2);
+        safeAreaRectTransform.sizeDelta = new Vector2(Screen.safeArea.width, Screen.safeArea.height);
+    }
     public void AddDialogText(bool twoParts, string textToAddPart1, string textToAddPart2 = null)
     {
         if (!twoParts)
@@ -19,5 +34,61 @@ public class ChatBubble : MonoBehaviour
             TextAreas[0].text = textToAddPart1;
             TextAreas[1].text = textToAddPart2;
         }
+
+        if (!initialized)
+        {
+            Initialize();
+            initialized = true;
+        }
+        CheckOverlap(rectTransform,safeAreaRectTransform);
     }
+
+    void CheckOverlap(RectTransform imageTransform, RectTransform parentRectTransform)
+    {
+        // Calculate the corners of the image and parent RectTransform in world space
+        Vector3[] imageCorners = new Vector3[4];
+        imageTransform.GetWorldCorners(imageCorners);
+
+        Vector3[] parentCorners = new Vector3[4];
+        parentRectTransform.GetWorldCorners(parentCorners);
+
+        // Check if any of the image corners are outside the parent RectTransform
+        bool isOutside = false;
+        foreach (Vector3 imageCorner in imageCorners)
+        {
+            if (!RectTransformUtility.RectangleContainsScreenPoint(parentRectTransform, imageCorner))
+            {
+                Debug.Log("CHAT OUTSIDE");
+                isOutside = true;
+                break;
+            }
+        }
+
+        // If the image is outside the parent RectTransform, move it back inside
+        if (isOutside)
+        {
+            Vector3 newPosition = imageTransform.position;
+
+            if (imageCorners[0].x < parentCorners[0].x) // Left edge outside
+            {
+                newPosition.x += parentCorners[0].x - imageCorners[0].x;
+            }
+            else if (imageCorners[2].x > parentCorners[2].x) // Right edge outside
+            {
+                newPosition.x -= imageCorners[2].x - parentCorners[2].x;
+            }
+
+            if (imageCorners[0].y < parentCorners[0].y) // Bottom edge outside
+            {
+                newPosition.y += parentCorners[0].y - imageCorners[0].y;
+            }
+            else if (imageCorners[1].y > parentCorners[1].y) // Top edge outside
+            {
+                newPosition.y -= imageCorners[1].y - parentCorners[1].y;
+            }
+
+            imageTransform.position = newPosition;
+        }
+    }
+    
 }
--- Assets/Scripts/NPCDialog.cs
+++ Assets/Scripts/NPCDialog.cs
@@ -17,33 +17,53 @@ public class NPCDialog : MonoBehaviour
         if (!hasTwoParts)   // Use simple chat bubble
         {
             ChatBubbles[0].gameObject.SetActive(true);
+            ChatBubbles[0].AddDialogText(false,textToDisplayPart1);
             if (staging == Staging.BackgroundRight
                 || staging == Staging.ForegroundRight
                 || staging == Staging.BackgroundCenter
                 || staging == Staging.ForegroundCenter)
             {
                 // Flip the bubble for staging on the right or Center
-                //Flip(ChatBubbles[0].gameObject);
+                Flip(ChatBubbles[0].gameObject,false);
             }
-            ChatBubbles[0].AddDialogText(false,textToDisplayPart1);
+            else
+            {
+                Flip(ChatBubbles[0].gameObject,true);
+            }
         }
         else  // Use dual chat bubble
         {
             ChatBubbles[1].gameObject.SetActive(true);
+            ChatBubbles[1].AddDialogText(true,textToDisplayPart1,textToDisplayPart2);
             if (staging == Staging.BackgroundLeft
                 || staging == Staging.ForegroundLeft)
             {
                 // Flip the bubble for staging on the right or Center
-                Flip(ChatBubbles[0].gameObject);
+                Flip(ChatBubbles[0].gameObject,false);
             }
-            ChatBubbles[1].AddDialogText(true,textToDisplayPart1,textToDisplayPart2);
+            else
+            {
+                Flip(ChatBubbles[0].gameObject,true);
+            }
         }
     }
     
-    void Flip(GameObject bubble)
+    void Flip(GameObject bubble, bool normal)
     {
+        int flipValue = -1;
+        if (normal)
+        {
+            flipValue = 1;
+        }
         Vector3 newScale = bubble.transform.localScale;
-        newScale.y *= -1;
+        newScale.x = flipValue;
         bubble.transform.localScale = newScale;
+
+        foreach (Transform child in bubble.transform)
+        {
+            Vector3 childScale = child.localScale;
+            childScale.x = flipValue;
+            child.localScale = childScale;
+        }
     }
 }
